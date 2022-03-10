50fc53a92a453856407cbc0d4e45522ea79e50f8
joshualectric <joshuainrecovery7@gmail.com>
2022-03-10 14:15:58 -0600

    completed most of backend

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
 //import routes
-
 const authRoute = require("./routes/auth");
+const toDosRoute = require("./routes/todos");
 
 const app =  express();
 
@@ -19,6 +19,7 @@ app.get("/api", (req, res) => {
 
 
 app.use("/api/auth", authRoute);
+app.use("/api/todos", toDosRoute);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to database");
