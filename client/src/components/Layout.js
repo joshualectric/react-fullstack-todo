755c6009e8d3909eafcf2903dd7a370a5c150fec
joshualectric <joshuainrecovery7@gmail.com>
2022-03-13 20:07:44 -0500

    front end

---
+++ client/src/components/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './Header';
+import AuthBox from './AuthBox';
+import { useGlobalContext } from '../context/GlobalContext';
+import Dashboard from './Dashboard';
+
+const Layout = () => {
+    const { fetchingUser } = useGlobalContext();
+    return  fetchingUser ? (
+        <div className='loading'>
+            <h1>Loading</h1>
+        </div>
+    ) : (
+        <BrowserRouter>
+            <Header />
+
+            <Routes>
+                <Route exact path="/" element={<AuthBox />} />
+                <Route path="/register" element={<AuthBox register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Layout;
